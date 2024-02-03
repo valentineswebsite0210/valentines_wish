ae65c85091d95d20efdf2f4059faa42b9babe437
valentineswebsite0210 <158774014+valentineswebsite0210@users.noreply.github.com>
2024-02-04 01:40:11 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,28 +1,32 @@
-// server.js
 const express = require('express');
 const nodemailer = require('nodemailer');
+const cors = require('cors'); // Add this line
 const app = express();
 const port = 3000;
 
 app.use(express.urlencoded({ extended: true }));
+app.use(cors()); // Enable CORS for all routes
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: 'valentineswebsite0210@gmail.com',
     pass: 'abry hjpf fnoo iuxw',
   },
 });
 
+// Add the CORS headers for the sendEmail route
+app.options('/sendEmail', cors()); // Enable preflight request
+
 app.post('/sendEmail', (req, res) => {
   // Handle the email sending logic here
   const mailOptions = {
     from: 'valentineswebsite0210@gmail.com',
     to: 'valentineswebsite0210@gmail.com',
-    subject: 'Valentine\'s Day Confirmation',
+    subject: "Valentine's Day Confirmation",
     text: 'Yay! They said yes to being your valentine! 💖',
   };
 
